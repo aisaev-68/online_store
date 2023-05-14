635a65f1b847a5a4b967470c0afe4673c1088781
ramazan_aisaev <aisaev-68@yandex.ru>
2023-05-14 13:03:02 +0300

    1

--- megano/megano/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from catalog.views import CategoryView, TagsView, BannersView, CatalogView
-
-urlpatterns = [
-    path("api/categories/", CategoryView.as_view()),
-    path("api/banners/", BannersView.as_view()),
-    path("api/tags/", TagsView.as_view()),
-    path("api/catalog/", CatalogView.as_view()),
-]
--- megano/megano/orders/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from orders.models import Order
-
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'user', 'totalCost', 'createdAt', 'status']
-    list_display_links = ['pk', 'user']
