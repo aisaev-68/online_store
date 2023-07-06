4a676cd480122120cac0f61b0da614819093cf6f
ramazan_aisaev <aisaev-68@yandex.ru>
2023-07-06 22:16:53 +0300

    fix

--- diplom_store/online_store/task.py
+++ diplom_store/online_store/task.py
@@ -9,9 +9,7 @@ for i in range(8):
     j = 0
     for rownum in range(sheet.nrows):
         new_row = []
-        new_row_2 = []
         row = sheet.row_values(rownum)
-
         row_str = str(row[1])
         if not row_str.startswith('ФЕР'):
             if str(row[1]).startswith('ТЦ'):
@@ -21,17 +19,14 @@ for i in range(8):
                 new_row.append(row[5])
                 new_row.append(row[8])
                 new_row.append(row[9])
-                print(new_row)
-                # print(price[2])
+                j += 1
             elif '-' in str(row[1]) and str(row[1]).split('.')[0].isdigit():
                 new_row.append(row[2])
                 new_row.append(row[5])
                 new_row.append(row[8])
                 new_row.append(row[9])
-                print(new_row)
-        for ind, item in enumerate(new_row):
-            ws.write(j, ind, item)
-        j += 1
+                j += 1
+            for ind, item in enumerate(new_row):
+                ws.write(j - 1, ind, item)
+
 wb.save('new06.xls')
-    # for c_el in row:
-    #     print(c_el)
