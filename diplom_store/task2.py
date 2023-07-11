e4d6b39e47225551bc239a9e0e29ea66a23e2dfb
ramazan_aisaev <aisaev-68@yandex.ru>
2023-07-11 17:22:34 +0300

    fix

--- diplom_store/task2.py
+++ diplom_store/task2.py
@@ -9,33 +9,29 @@ wb = xlwt.Workbook()
 for shet_item in rb.get_sheet_names():
     sheet = rb.get_sheet_by_name(shet_item)
     ws = wb.add_sheet(shet_item)
-    # print('99999999', sheet.max_row)
 
     j = 0
     for i in range(1, sheet.max_row):
         new_row = []
         vid_tovara = sheet.cell(row=i, column=2).value
-        if str(vid_tovara).startswith('ТЦ') or str(vid_tovara).startswith('ФССЦ') or str(vid_tovara).startswith(
-                'Из состава'):
+        # print(vid_tovara)
+        if str(vid_tovara).startswith('ТЦ') or str(vid_tovara).startswith('ФССЦ'):
             price = str(sheet.cell(row=i + 2, column=3).value)
+            print(price)
             if price.split('/')[0][:4] == 'Цена':
                 price = price.split('/')[0][5:]
-            if 'Объем' in price:
-                price = str(sheet.cell(row=i + 3, column=3).value)
-                price = price.split('/')[0][5:]
-            if 'Всего по позиции' in price:
+
+            if 'Объем' in price or 'Всего по позиции' in price:
                 price = str(sheet.cell(row=i, column=10).value)
-            # print(sheet.cell(row=i, column=1).value,
-            #       sheet.cell(row=i, column=3).value,
-            #       sheet.cell(row=i, column=6).value,
-            #       sheet.cell(row=i, column=9).value,
-            #       price)
+                # print(price)
+
             j += 1
             new_row.append(str(sheet.cell(row=i, column=1).value).replace('О', '').rstrip())
             new_row.append(sheet.cell(row=i, column=3).value)
             new_row.append(sheet.cell(row=i, column=6).value)
             new_row.append(sheet.cell(row=i, column=9).value)
             new_row.append(price)
+            print(new_row)
             for ind, item in enumerate(new_row):
                 ws.write(j - 1, ind, item)
 wb.save('new_most.xls')
